19e5ea52faf14ec791a6a4359ee05f66f783d2b4
RKanna <rkannan.balakrishnan@hotmail.com>
2024-01-01 08:54:32 +0530

    backend-server

---
+++ Login/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UsersModel = require("../Models/Users.js");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/BlogFullStackDB");
+
+//For user Login
+app.post(`/login`, (req, res) => {
+  const { email, password } = req.body;
+  UsersModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("success");
+      } else {
+        res.json("The password is incorrect");
+      }
+    } else {
+      res.json("The Email is not Registered with us");
+    }
+  });
+});
+
+//For user Registration
+app.post(`/Users`, (req, res) => {
+  const { email } = req.body;
+  UsersModel.findOne({ email: email }).then((existingUser) => {
+    if (existingUser) {
+      res.status(400).json("Email already exists");
+    } else {
+      UsersModel.create(req.body)
+        .then((user) => res.json({ message: "Signup success", user }))
+        .catch((err) => res.status(400).json(err));
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("server is running");
+});
